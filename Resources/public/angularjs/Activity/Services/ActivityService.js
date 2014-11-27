1d224dc9c45bd3f6c64c9859e763697b0816c801
Elorfin <penin.axel@gmail.com>
2014-11-27 11:17:34 +0100

    fix structure

--- Resources/public/angularjs/Activity/Services/ActivityService.js
+++ Resources/public/angularjs/Activity/Services/ActivityService.js
@@ -2,13 +2,27 @@
     'use strict';
 
     angular.module('Activity').factory('ActivityService', [
-        '$scope',
         '$http',
         function ($http) {
             return {
                 save: function (activity) {
-                    console.log($scope.activity);
-                    // AJAX request
+                    // Init
+                    var data = {
+                        typology   : activity.typology,
+                        description: activity.description
+                    };
+
+                    $http({
+                        method: 'POST',
+                        url: Routing.generate('activity_sequence_add_activity'),
+                        data: data
+                    })
+                    .success(function (data) {
+
+                    })
+                    .error(function(data, status) {
+
+                    });
                 }
             };
         }
