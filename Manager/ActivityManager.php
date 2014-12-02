319063c41ec59a1974a9f3ca90c9cbc2f7ef921c
Eric VINCENT <ericvincenterv@gmail.com>
2014-12-02 11:33:23 +0100

    Ajout root symfony et début route Ajax.

---
+++ Manager/ActivityManager.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Innova\ActivityBundle\Manager;
+
+use Innova\ActivityBundle\Entity\AbstractActivity;
+use JMS\DiExtraBundle\Annotation as DI;
+use Innova\ActivityBundle\Entity\Activity;
+
+/**
+ * @DI\Service("innova.manager.activity_manager")
+ */
+class ActivityManager
+{
+    /**
+    * @DI\InjectParams({
+    *   "container" = @DI\Inject("service_container")
+    * })
+    */
+    public function __construct($container)
+    {
+        $this->container = $container;
+        $this->em = $this->container->get('claroline.persistence.object_manager');
+    }
+
+    public function countActivities(Activity $activity){
+        if(!$count = $activity->getActivities()){
+            $count = 1;
+        }
+
+        return count($count) + 1;
+    }
+
+    public function addActivity(Activity $activity){
+
+        $activityType = "ActivityQRU"; // For tests. Eric.
+        $activity = $this->createActivity($activity, $activityType);
+//        $activity = $this->createActivity($activitySequence);
+
+        return $activity;
+    }
+
+    public function createActivity(Activity $activity, $activityType)
+//    public function createActivity(ActivitySequence $activitySequence)
+     {
+//        $activityType = "ActivityVF";
+
+        $activity = $this->em->factory('Innova\ActivityBundle\Entity\\' . $activityType);
+//        $activity = new ActivityVF;
+        $activity->setName("New Activity");
+        $activity->setDescription("New Description");
+        $activity->setActivitySequence($activitySequence);
+        $activity->setPosition(1);
+        $this->em->persist($activity);
+        $this->em->flush();
+
+        return $activity;
+    }
+
+    public function deleteActivity(AbstractActivity $activity) {
+        $activity = $activity->getActivity();
+        $this->em->remove($activity);
+        $this->em->flush();
+
+        $this->reorderActivity($activity);
+
+        return $activity;
+    }
+
+    public function reorderActivity(Activity $activity){
+        if ($activities = $activity->getActivities() ) {
+            $i = 1;
+            foreach ($activities as $activity) {
+                $activity->setOrder($i);
+                $this->em->persist($activity);
+                $i++;
+            }
+            $this->em->flush();
+        }
+    }
+
+    public function activityToJson(Activity $activity)
+    {
+        $activityActivities = array ();
+        if ($activities = $activity->getActivities() ) {
+            foreach ($activities as $activity) {
+                $activityActivities[] = array (
+                                                    "id" => $activity->getId(),
+                                                    "name" => $activity->getName(),
+                                                    "order" => $activity->getPosition()
+                );
+            }
+        }
+
+        $activityAttrs = array (
+            "id"         => $activity->getId(),
+            "name"       => $activity->getResourceNode()->getName(),
+            "activities" => $activityActivities,
+        );
+
+        return json_encode($activityAttrs);
+    }
+
+
+    public function activityAttrs(AbstractActivity $activity)
+    {
+        $activityAttrs = array (
+                            "id" => $activity->getId(),
+                            "name" => $activity->getName(),
+                            "order" => $activity->getPosition()
+        );
+
+        return $activityAttrs;
+    }
+
+    public function applyOrder(ActivitySequence $activitySequence, $order)
+    {
+        $i=1;
+        $order = json_decode($order);
+        foreach ($order as $activityId) {
+            $activity = $this->em->getRepository('InnovaActivityBundle:Activity')->find($activityId);
+            $activity->setOrder($i);
+            $this->em->persist($activity);
+            $i++;
+        }
+        $this->em->flush();
+
+        return $activitySequence;
+    }
+}
--- Resources/public/angularjs/Activity/Services/ActivityService.js
+++ Resources/public/angularjs/Activity/Services/ActivityService.js
@@ -20,6 +20,23 @@
                     })
                     .success(function (data) {
                     })
+
+
+                    addActivity: function() {
+                        var deferred = $q.defer();
+                        deferred.notify();
+                        LoaderService.startRequest();
+                        $http.get(Routing.generate('activity_add_activity', { activityId: activity.id }))
+                        .success(function (data) {
+                            deferred.resolve(data.activity);
+                            activitySequence.activities.push(data.activity);
+                            LoaderService.endRequest();
+                        });
+
+                        return deferred.promise;
+                    },
+
+
                     .error(function(data, status) {
 //                        AlertFactory.addAlert('danger', 'Error while adding activity.');
                     });
