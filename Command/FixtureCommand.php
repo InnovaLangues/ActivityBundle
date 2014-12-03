db61a8b390392a51656c5e4130d5828c03cbe9c7
Eric VINCENT <ericvincenterv@gmail.com>
2014-12-03 15:15:38 +0100

    Add ActivityTypeAvailable fixtures.

--- Command/FixtureCommand.php
+++ Command/FixtureCommand.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-use Innova\SelfBundle\Entity\ActivityTypeAvailable;
+use Innova\ActivityBundle\Entity\ActivityTypeAvailable;
 
 class FixtureCommand extends ContainerAwareCommand
 {
@@ -25,34 +25,22 @@ class FixtureCommand extends ContainerAwareCommand
             $em = $this->getContainer()->get('doctrine')->getEntityManager('default');
 
             /* TYPOLOGY */
-            $typologies = array(
-                array("TVF", "Vrai-Faux"), array("TQRU", "Question à Réponse Unique"),
-                array("TQRM", "Question à Réponses Multiples"), array("TLCMLDM", "Liste de mots"),
-                array("APP", "Appariemment"), array("TVFNM", "Vrai-Faux-Non Mentionné"),
-                array("TLCMLMULT", "Listes de choix multiple"), array("TLQROC", "Question Réponse Ouverte Courte")
+            $activityTypeAvailables = array(
+                array("A", "A1"), array("B", "B1"),
+                array("C", "C1")
             );
-/*
-            foreach ($typologies as $typology) {
-
-                if (!$typo = $em->getRepository('InnovaSelfBundle:Typology')->findOneByName($typology[0])) {
-                    $typo = new Typology();
-                    $typo->setName($typology[0]);
-                    $typo->setDescription($typology[1]);
-                    $em->persist($typo);
-                    $output->writeln("Add new Typology (".$typology[0]." : ".$typology[1].").");
-                } else {
-                    if ($typo->getDescription() != $typology[1]) {
-                        $typo->setDescription($typology[1]);
-                        $em->persist($typo);
-                        $output->writeln("Edit ".$typology[0]." description (".$typology[1].").");
-                    }
-                }
+            foreach ($activityTypeAvailables as $activityTypeAvailable) {
+                $activityType = new ActivityTypeAvailable();
+                $activityType->setName($activityTypeAvailable[0]);
+                $activityType->setClass($activityTypeAvailable[1]);
+                $em->persist($activityType);
+                $output->writeln("Add new activityTypeAvailable (".$activityTypeAvailable[0]." : ".$activityTypeAvailable[1].").");
             }
             $em->flush();
- */
+
             $now = time();
             $duration = $now - $start;
 
-            $output->writeln("Fixtures exécutées en ".$duration." sec.");
+            $output->writeln("Fixtures exécutées en ". $duration." sec.");
     }
 }
