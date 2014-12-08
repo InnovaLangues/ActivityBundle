679c39c51972c61eeae6247aa7616a3f91f74256
Eric VINCENT <ericvincenterv@gmail.com>
2014-12-08 16:53:35 +0100

    Updating Path to Activity and fix.

--- Form/Type/ActivityType.php
+++ Form/Type/ActivityType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class PathType extends AbstractType
+class ActivityType extends AbstractType
 {
     // A faire pour les autres données ? information, objet, proposition, question ?
 
