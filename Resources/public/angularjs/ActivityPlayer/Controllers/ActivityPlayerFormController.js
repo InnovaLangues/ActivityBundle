ad31f432c65a740b65ca092da26e23852c823a9d
eldoniel <nicofourdu@hotmail.fr>
2015-04-29 15:48:23 +0200

    Random order now working on choices in the player (can trigger an error, see details in comments)

--- Resources/public/angularjs/ActivityPlayer/Controllers/ActivityPlayerFormController.js
+++ Resources/public/angularjs/ActivityPlayer/Controllers/ActivityPlayerFormController.js
@@ -12,6 +12,17 @@
             
             this.iterator = 0;
             
+            this.randomSort = function(choice) {
+                this.value = null;
+                if (this.sequence.activities[this.iterator].type.randomlyOrdered) {
+                    this.value = Math.random();
+                }
+                else {
+                    this.value = "id";
+                }
+                return this.value;
+            }.bind(this);
+            
             this.next = function () {
                 this.iterator = this.iterator + 1;
             };
