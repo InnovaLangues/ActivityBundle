4b43855b8a932efab327718d26ebe6e088b520cf
Nicolas Dufour <nico@nico>
2015-05-26 11:15:09 +0200

    upgrade claro 5

--- InnovaActivityBundle.php
+++ InnovaActivityBundle.php
@@ -6,6 +6,7 @@ use Claroline\CoreBundle\Library\PluginBundle;
 use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
 use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 use Claroline\KernelBundle\Bundle\ConfigurationProviderInterface;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Innova Activity Bundle
