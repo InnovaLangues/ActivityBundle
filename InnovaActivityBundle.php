6649aef7a470ea1b3ed690899036cb8a576b78b2
Nicolas Dufour <nico@nico>
2015-05-26 11:05:07 +0200

    Upgrade Claro 5

--- InnovaActivityBundle.php
+++ InnovaActivityBundle.php
@@ -5,11 +5,12 @@ namespace Innova\ActivityBundle;
 use Claroline\CoreBundle\Library\PluginBundle;
 use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
 use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
+use Claroline\KernelBundle\Bundle\ConfigurationProviderInterface;
 
 /**
  * Innova Activity Bundle
  */
-class InnovaActivityBundle extends PluginBundle implements AutoConfigurableInterface
+class InnovaActivityBundle extends PluginBundle implements AutoConfigurableInterface, ConfigurationProviderInterface
 {
     public function supports($environment)
     {
@@ -26,5 +27,23 @@ class InnovaActivityBundle extends PluginBundle implements AutoConfigurableInter
         $config = new ConfigurationBuilder();
 
         return $config->addRoutingResource(__DIR__ . '/Resources/config/routing.yml', null, 'innova_activity');
+    }
+    
+    public function suggestConfigurationFor(Bundle $bundle, $environment)
+    {
+        $bundleClass = get_class($bundle);
+        $config = new ConfigurationBuilder();
+        $emptyConfigs = array(
+            'Innova\AngularJSBundle\InnovaAngularJSBundle',
+            'Innova\AngularUIBootstrapBundle\InnovaAngularUIBootstrapBundle',
+            'Innova\AngularUITranslationBundle\InnovaAngularUITranslationBundle',
+            'Innova\AngularUIResourcePickerBundle\InnovaAngularUIResourcePickerBundle',
+            'Innova\AngularUITinyMCEBundle\InnovaAngularUITinyMCEBundle',
+            'Innova\AngularUISortableBundle\InnovaAngularUISortableBundle',
+        );
+        if (in_array($bundleClass, $emptyConfigs)) {
+            return $config;
+        }
+        return false;
     }
 }
